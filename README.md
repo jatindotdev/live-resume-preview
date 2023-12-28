a1aa8d5c0e3bbe93823c46fb5ecd29791f76207e
Jatin Kumar <main.jatink@gmail.com>
2023-12-28 21:20:35 +0530

    feat: remix * felt like doing it

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-# Minimalist CV
+# Welcome to Remix!
 
-Simple web app that renders minimalist CV with print-friendly layout.
+- [Remix Docs](https://remix.run/docs)
 
-Built with Next.js and shadcn/ui, deployed on Vercel.
+## Development
 
-# License
+From your terminal:
 
-[MIT](https://choosealicense.com/licenses/mit/)
+```sh
+npm run dev
+```
+
+This starts your app in development mode, rebuilding assets on file changes.
+
+## Deployment
+
+First, build your app for production:
+
+```sh
+npm run build
+```
+
+Then run the app in production mode:
+
+```sh
+npm start
+```
+
+Now you'll need to pick a host to deploy it to.
+
+### DIY
+
+If you're familiar with deploying node applications, the built-in Remix app server is production-ready.
+
+Make sure to deploy the output of `remix build`
+
+- `build/`
+- `public/build/`
---
+++ app/components/project-card.tsx
@@ -0,0 +1,68 @@
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "./ui/card";
+import { Badge } from "./ui/badge";
+import { ResumeData } from "~/lib/types";
+
+type Props = ResumeData["projects"][number];
+
+export function ProjectCard({ title, description, techStack, link }: Props) {
+  return (
+    <Card className="flex flex-col overflow-hidden border border-muted">
+      <CardHeader>
+        <div className="space-y-1">
+          <CardTitle className="text-base">
+            {link ? (
+              <a
+                href={link.href}
+                target="_blank"
+                className="inline-flex items-center gap-1 hover:underline"
+              >
+                {title}{" "}
+                <span className="h-1 w-1 rounded-full bg-green-500"></span>
+              </a>
+            ) : (
+              title
+            )}
+          </CardTitle>
+          <div className="hidden font-mono text-xs underline print:visible">
+            {link?.href
+              .replace("https://", "")
+              .replace("www.", "")
+              .replace("/", "")}
+          </div>
+          <CardDescription className="font-mono text-xs">
+            {Array.isArray(description) ? (
+              <ul className="my-2 ml-4 list-disc">
+                {description.map((description) => (
+                  <li className="mt-1" key={description}>
+                    {description}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              description
+            )}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="mt-auto flex">
+        <div className="mt-2 flex flex-wrap gap-1">
+          {techStack.map((stack) => (
+            <Badge
+              className="px-1 py-0 text-[10px]"
+              variant="secondary"
+              key={stack}
+            >
+              {stack}
+            </Badge>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
